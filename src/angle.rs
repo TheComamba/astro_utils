027f1717ee3cdf6293f7aa0ad0fa477650cfec50
TheComamba <98019967+TheComamba@users.noreply.github.com>
2023-12-28 05:57:37 +0100

    Dev/kepler orbit utils (#2)

--- src/angle.rs
+++ src/angle.rs
@@ -7,25 +7,35 @@ use crate::{Float, PI, TWO_PI};
 
 static RADIANS_PER_DEGREE: Float = PI / 180.0;
 static DEGREES_PER_RADIAN: Float = 1.0 / RADIANS_PER_DEGREE;
+static ARCSECS_PER_RADIAN: Float = 3600.0 * DEGREES_PER_RADIAN;
+static RADIAN_PER_ARCSEC: Float = 1.0 / ARCSECS_PER_RADIAN;
 
 #[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
 pub struct Angle {
     radian: Float,
 }
 
+pub enum Normalization {
+    ZeroToTwoPi,
+    MinusPiToPi,
+    MinusPiHalfToPiHalf,
+}
+
 impl Angle {
-    pub fn from_radians(radian: Float) -> Angle {
-        let mut angle = Angle { radian };
-        angle.normalize();
-        angle
+    pub const fn from_radians(radian: Float) -> Angle {
+        Angle { radian }
     }
 
     pub fn from_degrees(degree: Float) -> Angle {
-        let mut angle = Angle {
+        Angle {
             radian: degree * RADIANS_PER_DEGREE,
-        };
-        angle.normalize();
-        angle
+        }
+    }
+
+    pub fn from_arcsecs(arcsec: Float) -> Angle {
+        Angle {
+            radian: arcsec * RADIAN_PER_ARCSEC,
+        }
     }
 
     pub fn as_radians(&self) -> Float {
@@ -36,22 +46,48 @@ impl Angle {
         self.radian * DEGREES_PER_RADIAN
     }
 
-    pub fn eq_within(&self, other: Angle, relative_accuracy: Float) -> bool {
-        let max = if self.radian > other.radian {
-            self.radian.abs()
-        } else {
-            other.radian.abs()
-        };
-        let diff = (self.radian - other.radian).abs();
-        diff <= max * relative_accuracy
+    pub fn as_arcsecs(&self) -> Float {
+        self.radian * ARCSECS_PER_RADIAN
     }
 
-    fn normalize(&mut self) {
-        while self.radian < 0.0 {
-            self.radian += TWO_PI;
-        }
-        while self.radian >= TWO_PI {
-            self.radian -= TWO_PI;
+    pub fn eq_within(&self, other: Angle, relative_accuracy: Float) -> bool {
+        let abs_accuracy = relative_accuracy * TWO_PI;
+        let diff = self.radian - other.radian;
+        let diff = diff % TWO_PI;
+        let diff = if diff > PI {
+            diff - TWO_PI
+        } else if diff < -PI {
+            diff + TWO_PI
+        } else {
+            diff
+        };
+        diff.abs() <= abs_accuracy
+    }
+
+    pub fn normalize(&mut self, normalization: Normalization) {
+        match normalization {
+            Normalization::ZeroToTwoPi => {
+                self.radian = self.radian % TWO_PI;
+                if self.radian < 0.0 {
+                    self.radian += TWO_PI;
+                }
+            }
+            Normalization::MinusPiToPi => {
+                self.radian = self.radian % TWO_PI;
+                if self.radian > PI {
+                    self.radian -= TWO_PI;
+                } else if self.radian < -PI {
+                    self.radian += TWO_PI;
+                }
+            }
+            Normalization::MinusPiHalfToPiHalf => {
+                self.radian = self.radian % PI;
+                if self.radian > PI / 2.0 {
+                    self.radian -= PI;
+                } else if self.radian < -PI / 2.0 {
+                    self.radian += PI;
+                }
+            }
         }
     }
 }
@@ -74,7 +110,7 @@ impl Sub for Angle {
 
 impl Display for Angle {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{:.2} rad", self.as_radians())
+        write!(f, "{:.2} deg", self.as_degrees())
     }
 }
 
@@ -100,6 +136,32 @@ mod tests {
     }
 
     #[test]
+    fn test_arcsecs() {
+        let expected = Angle::from_radians(RADIAN_PER_ARCSEC);
+        let angle = Angle::from_arcsecs(1.0);
+        assert!(angle.eq_within(expected, TEST_ACCURACY));
+        assert!((angle.as_arcsecs() - 1.0).abs() < TEST_ACCURACY);
+    }
+
+    #[test]
+    fn quarter_circle_in_various_units() {
+        let radians = Angle::from_radians(PI / 2.0);
+        let degrees = Angle::from_degrees(90.0);
+        let arcsecs = Angle::from_arcsecs(90.0 * 3600.0);
+        assert!(radians.eq_within(degrees, TEST_ACCURACY));
+        assert!(radians.eq_within(arcsecs, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn half_circle_in_various_units() {
+        let radians = Angle::from_radians(PI);
+        let degrees = Angle::from_degrees(180.0);
+        let arcsecs = Angle::from_arcsecs(180.0 * 3600.0);
+        assert!(radians.eq_within(degrees, TEST_ACCURACY));
+        assert!(radians.eq_within(arcsecs, TEST_ACCURACY));
+    }
+
+    #[test]
     fn test_add() {
         let angle1 = Angle::from_radians(1.0);
         let angle2 = Angle::from_radians(2.0);
@@ -116,17 +178,125 @@ mod tests {
     }
 
     #[test]
-    fn test_moduloness() {
-        let angle1 = Angle::from_radians(1.0);
-        let angle2 = Angle::from_radians(TWO_PI + 1.0);
-        let angle3 = Angle::from_radians(-TWO_PI + 1.0);
-        assert!(angle1.eq_within(angle2, TEST_ACCURACY));
-        assert!(angle1.eq_within(angle3, TEST_ACCURACY));
+    fn test_normalization_range() {
+        for i in -100..100 {
+            let radians = TWO_PI / 5.0 * i as Float;
+            let mut angle = Angle::from_radians(radians);
+            angle.normalize(Normalization::ZeroToTwoPi);
+            println!(
+                "ZeroToTwoPi: {} deg got normalised to {}",
+                radians * DEGREES_PER_RADIAN,
+                angle
+            );
+            assert!(angle.as_radians() >= 0.0);
+            assert!(angle.as_radians() < TWO_PI);
+
+            let mut angle = Angle::from_radians(radians);
+            angle.normalize(Normalization::MinusPiToPi);
+            println!(
+                "MinusPiToPi: {} deg got normalised to {}",
+                radians * DEGREES_PER_RADIAN,
+                angle
+            );
+            assert!(angle.as_radians() >= -PI);
+            assert!(angle.as_radians() < PI);
+
+            let mut angle = Angle::from_radians(radians);
+            angle.normalize(Normalization::MinusPiHalfToPiHalf);
+            println!(
+                "MinusPiHalfToPiHalf: {} deg got normalised to {}",
+                radians * DEGREES_PER_RADIAN,
+                angle
+            );
+            assert!(angle.as_radians() >= -PI / 2.0);
+            assert!(angle.as_radians() < PI / 2.0);
+        }
+    }
+
+    #[test]
+    fn test_normalizing_quarter_pi() {
+        let mut angle = Angle::from_radians(PI / 4.0);
+        let expected = Angle::from_radians(PI / 4.0);
+        angle.normalize(Normalization::ZeroToTwoPi);
+        println!("ZeroToTwoPi: expected: {}, actual: {}", expected, angle);
+        assert!(angle.eq_within(expected, TEST_ACCURACY));
+
+        angle.normalize(Normalization::MinusPiToPi);
+        println!("MinusPiToPi: expected: {}, actual: {}", expected, angle);
+        assert!(angle.eq_within(expected, TEST_ACCURACY));
+
+        angle.normalize(Normalization::MinusPiHalfToPiHalf);
+        println!(
+            "MinusPiHalfToPiHalf: expected: {}, actual: {}",
+            expected, angle
+        );
+        assert!(angle.eq_within(expected, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn test_normalizing_three_quarters_pi() {
+        let mut angle = Angle::from_radians(3.0 * PI / 4.0);
+        let expected = Angle::from_radians(3.0 * PI / 4.0);
+        angle.normalize(Normalization::ZeroToTwoPi);
+        println!("ZeroToTwoPi: expected: {}, actual: {}", expected, angle);
+        assert!(angle.eq_within(expected, TEST_ACCURACY));
+
+        angle.normalize(Normalization::MinusPiToPi);
+        println!("MinusPiToPi: expected: {}, actual: {}", expected, angle);
+        assert!(angle.eq_within(expected, TEST_ACCURACY));
+
+        let expected = Angle::from_radians(-PI / 4.0);
+        angle.normalize(Normalization::MinusPiHalfToPiHalf);
+        println!(
+            "MinusPiHalfToPiHalf: expected: {}, actual: {}",
+            expected, angle
+        );
+        assert!(angle.eq_within(expected, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn test_normalizing_minus_quarter_pi() {
+        let mut angle = Angle::from_radians(-PI / 4.0);
+        let expected = Angle::from_radians(7.0 * PI / 4.0);
+        angle.normalize(Normalization::ZeroToTwoPi);
+        println!("ZeroToTwoPi: expected: {}, actual: {}", expected, angle);
+        assert!(angle.eq_within(expected, TEST_ACCURACY));
+
+        let expected = Angle::from_radians(-PI / 4.0);
+        angle.normalize(Normalization::MinusPiToPi);
+        println!("MinusPiToPi: expected: {}, actual: {}", expected, angle);
+        assert!(angle.eq_within(expected, TEST_ACCURACY));
+
+        angle.normalize(Normalization::MinusPiHalfToPiHalf);
+        println!(
+            "MinusPiHalfToPiHalf: expected: {}, actual: {}",
+            expected, angle
+        );
+        assert!(angle.eq_within(expected, TEST_ACCURACY));
     }
 
     #[test]
     fn test_display() {
-        let angle = Angle::from_radians(1.0);
-        assert_eq!(format!("{}", angle), "1.00 rad");
+        let angle = Angle::from_degrees(100.0);
+        assert_eq!(format!("{}", angle), "100.00 deg");
+    }
+
+    #[test]
+    fn test_eq_within() {
+        let starts = vec![-10.0, 0.0, 3.0, 7.0];
+        let small_diffs = vec![-TEST_ACCURACY / 100.0, 0.0, TEST_ACCURACY / 100.0];
+        let large_diffs = vec![-TWO_PI, 0.0, TWO_PI, 100.0 * TWO_PI];
+        for start in starts {
+            for small_diff in &small_diffs {
+                for large_diff in &large_diffs {
+                    let rad1 = start;
+                    let rad2 = start + small_diff + large_diff;
+                    let angle1 = Angle::from_radians(rad1);
+                    let angle2 = Angle::from_radians(rad2);
+                    println!("Expecting {} == {}", angle1, angle2);
+                    assert!(angle1.eq_within(angle2, TEST_ACCURACY));
+                }
+            }
+        }
     }
 }
---
+++ src/kepler_orbit.rs
@@ -0,0 +1,239 @@
+use crate::{angle::Angle, length::Length, mass::Mass, time::Time, Float, TWO_PI};
+
+/*
+ * The orbital period is the time it takes for a given object to make one full orbit around another object.
+ * https://en.wikipedia.org/wiki/Orbital_period
+ */
+pub fn orbital_period(semi_major_axis: Length, mass1: Mass, mass2: Mass) -> Time {
+    const G: Float = 6.67430e-11;
+
+    let semi_major_axis_cubed = semi_major_axis.as_meters().powi(3);
+    let total_mass = mass1.as_kilograms() + mass2.as_kilograms();
+    let orbital_period = TWO_PI * (semi_major_axis_cubed / total_mass / G).sqrt();
+    Time::from_seconds(orbital_period)
+}
+
+/*
+ * The mean anomaly is the angle between the direction of periapsis and the current position of the orbiting body,
+ * as seen from the center of the ellipse (the point around which the object orbits).
+ * https://en.wikipedia.org/wiki/Mean_anomaly
+ *
+ * Output is normalised to the range [0, 2π).
+ */
+pub fn mean_anomaly(orbital_period: Time, time: Time) -> Angle {
+    let mean_motion = TWO_PI / orbital_period.as_seconds();
+    let mean_anomaly = mean_motion * (time.as_seconds() % orbital_period.as_seconds());
+    let mut mean_anomaly = Angle::from_radians(mean_anomaly);
+    mean_anomaly.normalize(crate::angle::Normalization::ZeroToTwoPi);
+    mean_anomaly
+}
+
+/*
+ * The eccentric anomaly is the angle between the direction of periapsis and the current position of the orbiting body,
+ * as seen from the center of the ellipse (the point around which the object orbits).
+ * https://en.wikipedia.org/wiki/Eccentric_anomaly
+ */
+pub fn eccentric_anomaly(mean_anomaly: Angle, eccentricity: Float) -> Angle {
+    static ACCURACY: Float = 1e-6;
+    let mean_anomaly = mean_anomaly.as_radians();
+    let mut eccentric_anomaly = mean_anomaly;
+    let mut error = 10.0 * ACCURACY;
+    while error > ACCURACY {
+        let numerator = eccentric_anomaly - eccentricity * eccentric_anomaly.sin() - mean_anomaly;
+        let denominator = 1.0 - eccentricity * eccentric_anomaly.cos();
+        let next_correction = numerator / denominator;
+        let next_eccentric_anomaly = eccentric_anomaly - next_correction;
+        error = next_correction.abs();
+        eccentric_anomaly = next_eccentric_anomaly;
+    }
+
+    Angle::from_radians(eccentric_anomaly)
+}
+
+/*
+ * The true anomaly is the angle between the direction of periapsis and the current position of the orbiting body,
+ * as seen from the main focus of the ellipse (the point around which the object orbits).
+ * https://en.wikipedia.org/wiki/True_anomaly
+ */
+pub fn true_anomaly(eccentric_anomaly: Angle, eccentricity: Float) -> Angle {
+    let sqrt_arg = (1.0 + eccentricity) / (1.0 - eccentricity);
+    let artan_arg = (eccentric_anomaly.as_radians() / 2.0).tan() * sqrt_arg.sqrt();
+    let true_anomaly = 2.0 * artan_arg.atan();
+    Angle::from_radians(true_anomaly)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solar_system_data::{
+        EARTH_MASS, EARTH_SEMI_MAJOR_AXIS, JUPITER_MASS, JUPITER_SEMI_MAJOR_AXIS, MOON_MASS,
+        MOON_SEMI_MAJOR_AXIS, SUN_MASS,
+    };
+
+    use super::*;
+
+    static TEST_ACCURACY: Float = 1e-2;
+
+    #[test]
+    fn orbital_period_of_earth() {
+        let expected_orbital_period = Time::from_days(365.256);
+        let orbital_period = orbital_period(EARTH_SEMI_MAJOR_AXIS, EARTH_MASS, SUN_MASS);
+        println!("Expected orbital period: {}", expected_orbital_period);
+        println!("Calculated orbital period: {}", orbital_period);
+        assert!(orbital_period.eq_within(expected_orbital_period, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn orbital_period_of_jupiter() {
+        let expected_orbital_period = Time::from_days(4332.59);
+        let orbital_period = orbital_period(JUPITER_SEMI_MAJOR_AXIS, JUPITER_MASS, SUN_MASS);
+        println!("Expected orbital period: {}", expected_orbital_period);
+        println!("Calculated orbital period: {}", orbital_period);
+        assert!(orbital_period.eq_within(expected_orbital_period, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn orbital_period_of_moon() {
+        let expected_orbital_period = Time::from_days(27.321);
+        let orbital_period = orbital_period(MOON_SEMI_MAJOR_AXIS, MOON_MASS, EARTH_MASS);
+        println!("Expected orbital period: {}", expected_orbital_period);
+        println!("Calculated orbital period: {}", orbital_period);
+        assert!(orbital_period.eq_within(expected_orbital_period, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn mean_anomaly_a_quarter_circle() {
+        let expected_mean_anomaly = Angle::from_radians(TWO_PI / 4.0);
+        let mean_anomaly = mean_anomaly(Time::from_years(4.0), Time::from_years(1.0));
+        println!("Expected mean anomaly: {}", expected_mean_anomaly);
+        println!("Calculated mean anomaly: {}", mean_anomaly);
+        assert!(mean_anomaly.eq_within(expected_mean_anomaly, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn mean_anomaly_a_half_circle() {
+        let expected_mean_anomaly = Angle::from_radians(TWO_PI / 2.0);
+        let mean_anomaly = mean_anomaly(Time::from_years(4.0), Time::from_years(2.0));
+        println!("Expected mean anomaly: {}", expected_mean_anomaly);
+        println!("Calculated mean anomaly: {}", mean_anomaly);
+        assert!(mean_anomaly.eq_within(expected_mean_anomaly, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn mean_anomaly_three_quarters_circle() {
+        let expected_mean_anomaly = Angle::from_radians(TWO_PI * 3.0 / 4.0);
+        let mean_anomaly = mean_anomaly(Time::from_years(4.0), Time::from_years(-1.0));
+        println!("Expected mean anomaly: {}", expected_mean_anomaly);
+        println!("Calculated mean anomaly: {}", mean_anomaly);
+        assert!(mean_anomaly.eq_within(expected_mean_anomaly, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn mean_anomaly_is_stable_after_loads_of_revolutions() {
+        let expected_mean_anomaly = Angle::from_radians(TWO_PI / 4.0);
+        let passed_time = Time::from_years(1e5 + 0.25);
+        let mean_anomaly = mean_anomaly(Time::from_years(1.0), passed_time);
+        println!("Expected mean anomaly: {}", expected_mean_anomaly);
+        println!("Calculated mean anomaly: {}", mean_anomaly);
+        assert!(mean_anomaly.eq_within(expected_mean_anomaly, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn eccentric_anomaly_from_quarter_circle_mean_anomaly_and_zero_eccentricity() {
+        let expected_eccentric_anomaly = Angle::from_radians(TWO_PI / 4.0);
+        let eccentric_anomaly = eccentric_anomaly(Angle::from_radians(TWO_PI / 4.0), 0.0);
+        println!("Expected eccentric anomaly: {}", expected_eccentric_anomaly);
+        println!("Calculated eccentric anomaly: {}", eccentric_anomaly);
+        assert!(eccentric_anomaly.eq_within(expected_eccentric_anomaly, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn eccentric_anomaly_from_half_circle_mean_anomaly_and_zero_eccentricity() {
+        let expected_eccentric_anomaly = Angle::from_radians(TWO_PI / 2.0);
+        let eccentric_anomaly = eccentric_anomaly(Angle::from_radians(TWO_PI / 2.0), 0.0);
+        println!("Expected eccentric anomaly: {}", expected_eccentric_anomaly);
+        println!("Calculated eccentric anomaly: {}", eccentric_anomaly);
+        assert!(eccentric_anomaly.eq_within(expected_eccentric_anomaly, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn eccentric_anomaly_from_three_quarters_circle_mean_anomaly_and_zero_eccentricity() {
+        let expected_eccentric_anomaly = Angle::from_radians(TWO_PI * 3.0 / 4.0);
+        let eccentric_anomaly = eccentric_anomaly(Angle::from_radians(TWO_PI * 3.0 / 4.0), 0.0);
+        println!("Expected eccentric anomaly: {}", expected_eccentric_anomaly);
+        println!("Calculated eccentric anomaly: {}", eccentric_anomaly);
+        assert!(eccentric_anomaly.eq_within(expected_eccentric_anomaly, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn eccentric_anomaly_from_negative_quarter_circle_mean_anomaly_and_zero_eccentricity() {
+        let expected_eccentric_anomaly = Angle::from_radians(-TWO_PI / 4.0);
+        let eccentric_anomaly = eccentric_anomaly(Angle::from_radians(-TWO_PI / 4.0), 0.0);
+        println!("Expected eccentric anomaly: {}", expected_eccentric_anomaly);
+        println!("Calculated eccentric anomaly: {}", eccentric_anomaly);
+        assert!(eccentric_anomaly.eq_within(expected_eccentric_anomaly, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn eccentric_anomaly_from_quarter_circle_mean_anomaly_and_half_eccentricity() {
+        let expected_eccentric_anomaly = Angle::from_degrees(115.79362093315422);
+        let eccentric_anomaly = eccentric_anomaly(Angle::from_radians(TWO_PI / 4.0), 0.5);
+        println!("Expected eccentric anomaly: {}", expected_eccentric_anomaly);
+        println!("Calculated eccentric anomaly: {}", eccentric_anomaly);
+        assert!(eccentric_anomaly.eq_within(expected_eccentric_anomaly, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn eccentric_anomaly_from_half_circle_mean_anomaly_and_half_eccentricity() {
+        let expected_eccentric_anomaly = Angle::from_radians(TWO_PI / 2.0);
+        let eccentric_anomaly = eccentric_anomaly(Angle::from_radians(TWO_PI / 2.0), 0.5);
+        println!("Expected eccentric anomaly: {}", expected_eccentric_anomaly);
+        println!("Calculated eccentric anomaly: {}", eccentric_anomaly);
+        assert!(eccentric_anomaly.eq_within(expected_eccentric_anomaly, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn eccentric_anomaly_from_three_quarters_circle_mean_anomaly_and_half_eccentricity() {
+        let mut expected_eccentric_anomaly = Angle::from_degrees(-115.79362093315422);
+        expected_eccentric_anomaly.normalize(crate::angle::Normalization::ZeroToTwoPi);
+        let eccentric_anomaly = eccentric_anomaly(Angle::from_radians(TWO_PI * 3.0 / 4.0), 0.5);
+        println!("Expected eccentric anomaly: {}", expected_eccentric_anomaly);
+        println!("Calculated eccentric anomaly: {}", eccentric_anomaly);
+        assert!(eccentric_anomaly.eq_within(expected_eccentric_anomaly, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn eccentric_anomaly_from_negative_quarter_circle_mean_anomaly_and_half_eccentricity() {
+        let expected_eccentric_anomaly = Angle::from_degrees(-115.79362093315422);
+        let eccentric_anomaly = eccentric_anomaly(Angle::from_radians(-TWO_PI / 4.0), 0.5);
+        println!("Expected eccentric anomaly: {}", expected_eccentric_anomaly);
+        println!("Calculated eccentric anomaly: {}", eccentric_anomaly);
+        assert!(eccentric_anomaly.eq_within(expected_eccentric_anomaly, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn true_anomaly_from_quarter_circle_eccentric_anomaly_and_zero_eccentricity() {
+        let expected_true_anomaly = Angle::from_radians(TWO_PI / 4.0);
+        let true_anomaly = true_anomaly(Angle::from_radians(TWO_PI / 4.0), 0.0);
+        println!("Expected true anomaly: {}", expected_true_anomaly);
+        println!("Calculated true anomaly: {}", true_anomaly);
+        assert!(true_anomaly.eq_within(expected_true_anomaly, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn true_anomaly_from_half_circle_eccentric_anomaly_and_zero_eccentricity() {
+        let expected_true_anomaly = Angle::from_radians(TWO_PI / 2.0);
+        let true_anomaly = true_anomaly(Angle::from_radians(TWO_PI / 2.0), 0.0);
+        println!("Expected true anomaly: {}", expected_true_anomaly);
+        println!("Calculated true anomaly: {}", true_anomaly);
+        assert!(true_anomaly.eq_within(expected_true_anomaly, TEST_ACCURACY));
+    }
+
+    #[test]
+    fn true_anomaly_from_three_quarters_circle_eccentric_anomaly_and_zero_eccentricity() {
+        let expected_true_anomaly = Angle::from_radians(TWO_PI * 3.0 / 4.0);
+        let true_anomaly = true_anomaly(Angle::from_radians(TWO_PI * 3.0 / 4.0), 0.0);
+        println!("Expected true anomaly: {}", expected_true_anomaly);
+        println!("Calculated true anomaly: {}", true_anomaly);
+        assert!(true_anomaly.eq_within(expected_true_anomaly, TEST_ACCURACY));
+    }
+}
