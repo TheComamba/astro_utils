0f267860546e6fbe6387bea82a3a02b66b0aa3fc
TheComamba <thecomamba@jojoheinze.de>
2023-12-28 12:40:27 +0100

    Test for conversion from ecliptic to unit vector.

--- src/coordinates/unit_vector.rs
+++ src/coordinates/unit_vector.rs
@@ -1,5 +1,6 @@
-use super::cartesian::CartesianCoordinates;
-use crate::Float;
+use super::{cartesian::CartesianCoordinates, ecliptic::EclipticCoordinates};
+use crate::{units::length::Length, Float};
+use std::fmt::Display;
 
 pub const X_DIRECTION: UnitVector = UnitVector {
     x: 1.,
@@ -24,13 +25,21 @@ pub struct UnitVector {
 }
 
 impl UnitVector {
-    pub fn from_cartesian(coords: &CartesianCoordinates) -> Self {
+    pub(crate) fn from_cartesian(coords: &CartesianCoordinates) -> Self {
         let length = coords.length();
         UnitVector {
             x: coords.x() / length,
             y: coords.y() / length,
             z: coords.z() / length,
         }
+    }
+
+    pub(crate) fn from_ecliptic(ecliptic: &EclipticCoordinates) -> Self {
+        todo!()
+    }
+
+    pub(crate) fn to_cartesian(&self, length: Length) -> CartesianCoordinates {
+        CartesianCoordinates::new(self.x * length, self.y * length, self.z * length)
     }
 
     pub(crate) fn x(&self) -> Float {
@@ -43,5 +52,43 @@ impl UnitVector {
 
     pub(crate) fn z(&self) -> Float {
         self.z
+    }
+}
+
+impl Display for UnitVector {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "({:.2}, {:.2}, {:.2})", self.x, self.y, self.z)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tests::TEST_ACCURACY;
+
+    #[test]
+    fn from_ecliptic() {
+        let values = vec![-1., 1., 10.];
+        for x in values.iter() {
+            for y in values.iter() {
+                for z in values.iter() {
+                    let x = Length::from_meters(*x);
+                    let y = Length::from_meters(*y);
+                    let z = Length::from_meters(*z);
+                    let cartesian = CartesianCoordinates::new(x, y, z);
+                    let length = cartesian.length();
+                    let expected_x = x / length;
+                    let expected_y = y / length;
+                    let expected_z = z / length;
+
+                    let ecliptic = EclipticCoordinates::from_cartesian(&cartesian);
+                    let unit_vector = UnitVector::from_ecliptic(&ecliptic);
+
+                    assert!((unit_vector.x() - expected_x).abs() < TEST_ACCURACY);
+                    assert!((unit_vector.y() - expected_y).abs() < TEST_ACCURACY);
+                    assert!((unit_vector.z() - expected_z).abs() < TEST_ACCURACY);
+                }
+            }
+        }
     }
 }
