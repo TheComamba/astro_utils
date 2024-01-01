7e05a029a2f8f161b2f237b75e02eeaa0538f0e5
TheComamba <thecomamba@jojoheinze.de>
2024-01-01 21:01:28 +0100

    Introduced color matching functions.

---
+++ src/color_matching_functions.rs
@@ -0,0 +1,73 @@
+use crate::{
+    black_body::planck_radiant_emittance,
+    units::{length::Length, temperature::Temperature},
+    Float,
+};
+
+fn tilted_gaussian(lambda: Float, mean: Float, sigma1: Float, sigma2: Float) -> Float {
+    let nominator = -0.5 * (lambda - mean).powi(2);
+    if lambda < mean {
+        let denominator = sigma1.powi(2);
+        (nominator / denominator).exp()
+    } else {
+        let denominator = sigma2.powi(2);
+        (nominator / denominator).exp()
+    }
+}
+
+pub(crate) fn x_color_matching(lambda: Length) -> Float {
+    1.056 * tilted_gaussian(lambda.as_nanometers(), 599.8, 37.9, 31.0)
+        + 0.362 * tilted_gaussian(lambda.as_nanometers(), 442.0, 16.0, 26.7)
+        + -0.065 * tilted_gaussian(lambda.as_nanometers(), 501.1, 20.4, 26.2)
+}
+
+pub(crate) fn y_color_matching(lambda: Length) -> Float {
+    0.821 * tilted_gaussian(lambda.as_nanometers(), 568.8, 46.9, 40.5)
+        + 0.286 * tilted_gaussian(lambda.as_nanometers(), 530.9, 16.3, 31.1)
+}
+
+pub(crate) fn z_color_matching(lambda: Length) -> Float {
+    1.217 * tilted_gaussian(lambda.as_nanometers(), 437.0, 11.8, 36.0)
+        + 0.681 * tilted_gaussian(lambda.as_nanometers(), 459.0, 26.0, 13.8)
+}
+
+pub(crate) fn convolute_with_black_body(
+    fun: Box<dyn Fn(Length) -> Float>,
+    temperature: Temperature,
+) -> Float {
+    const STEP: Length = Length::from_meters(1e-9);
+    let mut sum = 0.;
+    let mut lambda = Length::from_nanometers(380.);
+    while lambda.as_nanometers() < 780. {
+        let value = fun(lambda);
+        let planck = planck_radiant_emittance(lambda, temperature);
+        sum += value * planck;
+        lambda += STEP;
+    }
+    sum * STEP.as_meters()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestExpectance {
+        wavelength: Length,
+        temperature: Temperature,
+        expected: Float,
+    }
+
+    #[test]
+    fn color_matching_functions_are_between_zero_and_two() {
+        let mut lambda = Length::from_nanometers(380.);
+        while lambda.as_nanometers() < 780. {
+            let x = x_color_matching(lambda);
+            let y = y_color_matching(lambda);
+            let z = z_color_matching(lambda);
+            assert!(x >= 0. && x <= 2.);
+            assert!(y >= 0. && y <= 2.);
+            assert!(z >= 0. && z <= 2.);
+            lambda += Length::from_nanometers(1.);
+        }
+    }
+}
--- src/units/color.rs
+++ src/units/color.rs
@@ -1,6 +1,6 @@
-use crate::Float;
+use crate::{color_matching_functions::*, Float};
 
-use super::temperature::Temperature;
+use super::{length::Length, temperature::Temperature};
 
 pub struct Color {
     red: Float,
@@ -8,16 +8,98 @@ pub struct Color {
     blue: Float,
 }
 
+pub(crate) struct CIEXYZColor {
+    x: Float,
+    y: Float,
+    z: Float,
+}
+
 impl Color {
     pub const fn from_rgb(red: Float, green: Float, blue: Float) -> Color {
         Color { red, green, blue }
     }
 
-    pub fn from_temperature(t: Temperature) -> Color {
-        todo!()
+    pub fn from_temperature(temperature: Temperature) -> Color {
+        let x_fun = Box::new(|lambda: Length| x_color_matching(lambda));
+        let y_fun = Box::new(|lambda: Length| y_color_matching(lambda));
+        let z_fun = Box::new(|lambda: Length| z_color_matching(lambda));
+        let x = convolute_with_black_body(x_fun, temperature);
+        let y = convolute_with_black_body(y_fun, temperature);
+        let z = convolute_with_black_body(z_fun, temperature);
+        let xyz = CIEXYZColor::from_xyz(x, y, z);
+        xyz.to_rgb()
     }
 
     pub const fn as_rgb(&self) -> (Float, Float, Float) {
         (self.red, self.green, self.blue)
+    }
+}
+
+/*
+ * https://engineering.purdue.edu/~bouman/ece637/notes/pdf/Tristimulus.pdf
+ * Page 15 dfines the transformation matrix from RGB to XYZ
+ */
+impl CIEXYZColor {
+    const M: [[Float; 3]; 3] = [
+        [0.490, 0.310, 0.200],
+        [0.177, 0.813, 0.010],
+        [0.000, 0.010, 0.990],
+    ];
+
+    const M_INV: [[Float; 3]; 3] = [
+        [2.36, -0.896, -0.469],
+        [-0.515, 1.425, 0.090],
+        [0.005, -0.014, 1.009],
+    ];
+
+    fn from_xyz(x: Float, y: Float, z: Float) -> CIEXYZColor {
+        CIEXYZColor { x, y, z }
+    }
+
+    fn to_rgb(&self) -> Color {
+        let r = Self::M[0][0] * self.x + Self::M[0][1] * self.y + Self::M[0][2] * self.z;
+        let g = Self::M[1][0] * self.x + Self::M[1][1] * self.y + Self::M[1][2] * self.z;
+        let b = Self::M[2][0] * self.x + Self::M[2][1] * self.y + Self::M[2][2] * self.z;
+        Color::from_rgb(r, g, b)
+    }
+
+    pub const fn as_xyz(&self) -> (Float, Float, Float) {
+        (self.x, self.y, self.z)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tests::TEST_ACCURACY;
+
+    #[test]
+    fn thousand_degrees_is_red() {
+        let color = Color::from_temperature(Temperature::from_kelvin(5000.0));
+        let (r, g, b) = color.as_rgb();
+        println!("r: {}, g: {}, b: {}", r, g, b);
+        assert!((r - 0.999).abs() < TEST_ACCURACY);
+        assert!((g - 0.999).abs() < TEST_ACCURACY);
+        assert!((b - 0.999).abs() < TEST_ACCURACY);
+    }
+
+    #[test]
+    fn six_thousand_degrees_is_white() {
+        let color = Color::from_temperature(Temperature::from_kelvin(6000.0));
+        let (r, g, b) = color.as_rgb();
+        println!("r: {}, g: {}, b: {}", r, g, b);
+        assert!((r - 1.0).abs() < TEST_ACCURACY);
+        assert!((g - 1.0).abs() < TEST_ACCURACY);
+        assert!((b - 1.0).abs() < TEST_ACCURACY);
+    }
+
+    #[test]
+    fn twelve_thousand_degrees_is_blue() {
+        let color = Color::from_temperature(Temperature::from_kelvin(10000.0));
+        let (r, g, b) = color.as_rgb();
+        println!("r: {}, g: {}, b: {}", r, g, b);
+        assert!((r - 0.999).abs() < TEST_ACCURACY);
+        assert!((g - 0.999).abs() < TEST_ACCURACY);
+        assert!((b - 1.0).abs() < TEST_ACCURACY);
     }
 }
