69d0f64c80d20eaa680e23fdb44710091f18e50d
TheComamba <thecomamba@jojoheinze.de>
2024-01-09 21:33:02 +0100

    Also storing parallax.

--- playground/synchronous_query.sh
+++ playground/synchronous_query.sh
@@ -4,7 +4,7 @@ req="https://gea.esac.esa.int/tap-server/tap/sync"
 req+="?REQUEST=doQuery"
 req+="&LANG=ADQL"
 req+="&FORMAT=json"
-req+="&QUERY=SELECT+ecl_lon,ecl_lat,phot_g_mean_mag,logg_gspphot,distance_gspphot"
+req+="&QUERY=SELECT+ecl_lon,ecl_lat,phot_g_mean_mag,logg_gspphot,distance_gspphot,parallax"
 req+="+FROM+gaiadr3.gaia_source"
 req+="+WHERE+phot_g_mean_mag+<+6.5"
 curl $req -o result.json
