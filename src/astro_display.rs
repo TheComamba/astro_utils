d774f7b403762756e79272bcff65857589008afd
TheComamba <thecomamba@jojoheinze.de>
2024-02-07 19:53:28 +0100

    Some template implementations for AstroDisplay.

--- src/astro_display.rs
+++ src/astro_display.rs
@@ -1,3 +1,24 @@
 pub trait AstroDisplay {
     fn astro_display(&self) -> String;
 }
+
+impl<T> AstroDisplay for Option<T>
+where
+    T: AstroDisplay,
+{
+    fn astro_display(&self) -> String {
+        match self {
+            Some(value) => value.astro_display(),
+            None => String::new(),
+        }
+    }
+}
+
+impl<T> AstroDisplay for &T
+where
+    T: AstroDisplay,
+{
+    fn astro_display(&self) -> String {
+        (*self).astro_display()
+    }
+}
