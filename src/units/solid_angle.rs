f3184eea94c8d9d2228faf649477518355fb4e32
TheComamba <thecomamba@jojoheinze.de>
2024-02-09 15:14:11 +0100

    Adding solid angle zero.

--- src/units/solid_angle.rs
+++ src/units/solid_angle.rs
@@ -1,6 +1,8 @@
 use crate::astro_display::AstroDisplay;
 use simple_si_units::geometry::SolidAngle;
 
+pub const SOLID_ANGLE_ZERO: SolidAngle<f64> = SolidAngle { sr: 0.0 };
+
 impl AstroDisplay for SolidAngle<f64> {
     fn astro_display(&self) -> String {
         format!("{:.2} sr", self.sr)
