6b4fe97f8b3d9b0fd83a98ef13af2c0a2504e3af
TheComamba <thecomamba@jojoheinze.de>
2025-03-29 16:16:00 +0100

    Fixing velocity.

--- src/units/velocity.rs
+++ src/units/velocity.rs
@@ -1,4 +1,7 @@
-use uom::si::f64::Velocity;
+use uom::si::{
+    f64::Velocity,
+    velocity::{kilometer_per_second, meter_per_second, speed_of_light_in_vacuum},
+};
 
 use super::DISPLAY_THRESHOLD;
 use crate::astro_display::AstroDisplay;
@@ -11,17 +14,19 @@ pub enum VelocityUnit {
 
 pub fn display_velocity_in_units(velocity: &Velocity, units: VelocityUnit) -> String {
     match units {
-        VelocityUnit::MetresPerSecond => format!("{:.2} m/s", velocity.to_mps()),
+        VelocityUnit::MetresPerSecond => format!("{:.2} m/s", velocity.get::<meter_per_second>()),
         VelocityUnit::KilometresPerSecond => {
             format!("{:.2} km/s", velocity.get::<kilometer_per_second>())
         }
-        VelocityUnit::SpeedOfLight => format!("{:.2} c", velocity.to_c()),
+        VelocityUnit::SpeedOfLight => {
+            format!("{:.2} c", velocity.get::<speed_of_light_in_vacuum>())
+        }
     }
 }
 
 impl AstroDisplay for Velocity {
     fn astro_display(&self) -> String {
-        let units = if self.to_c().abs() > DISPLAY_THRESHOLD {
+        let units = if self.get::<speed_of_light_in_vacuum>().abs() > DISPLAY_THRESHOLD {
             VelocityUnit::SpeedOfLight
         } else if self.get::<kilometer_per_second>().abs() > DISPLAY_THRESHOLD {
             VelocityUnit::KilometresPerSecond
@@ -38,31 +43,31 @@ mod tests {
 
     #[test]
     fn test_velocity_display() {
-        let velocity = Velocity::from_mps(1.23);
+        let velocity = Velocity::new::<meter_per_second>(1.23);
         assert_eq!(velocity.astro_display(), "1.23 m/s");
-        let velocity = Velocity::from_kmps(1.23);
+        let velocity = Velocity::new::<kilometer_per_second>(1.23);
         assert_eq!(velocity.astro_display(), "1.23 km/s");
-        let velocity = Velocity::from_c(1.23);
+        let velocity = Velocity::new::<speed_of_light_in_vacuum>(1.23);
         assert_eq!(velocity.astro_display(), "1.23 c");
     }
 
     #[test]
     fn test_velocity_negative_display() {
-        let velocity = Velocity::from_mps(-1.23);
+        let velocity = Velocity::new::<meter_per_second>(-1.23);
         assert_eq!(velocity.astro_display(), "-1.23 m/s");
-        let velocity = Velocity::from_kmps(-1.23);
+        let velocity = Velocity::new::<kilometer_per_second>(-1.23);
         assert_eq!(velocity.astro_display(), "-1.23 km/s");
-        let velocity = Velocity::from_c(-1.23);
+        let velocity = Velocity::new::<speed_of_light_in_vacuum>(-1.23);
         assert_eq!(velocity.astro_display(), "-1.23 c");
     }
 
     #[test]
     fn test_velocity_display_thresholds() {
-        let velocity = Velocity::from_mps(0.1);
+        let velocity = Velocity::new::<meter_per_second>(0.1);
         assert_eq!(velocity.astro_display(), "0.10 m/s");
-        let velocity = Velocity::from_kmps(0.1);
+        let velocity = Velocity::new::<kilometer_per_second>(0.1);
         assert_eq!(velocity.astro_display(), "0.10 km/s");
-        let velocity = Velocity::from_c(0.1);
+        let velocity = Velocity::new::<speed_of_light_in_vacuum>(0.1);
         assert_eq!(velocity.astro_display(), "0.10 c");
     }
 }
